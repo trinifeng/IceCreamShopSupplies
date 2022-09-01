33909c08c053cee562414efde347d0f25813f53d
TRINI FENG <trini.feng23@CLS-APP-04.local>
2022-09-01 08:00:39 -0500

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,11 @@
+import java.util.Arrays.*
+
+fun main() {
+    val iceCreamShopSupplies = arrayOf("cones", "bowls", "scoopers", "spoons", "ice cream machines", "hot fudge", "fruit", "cookie dough")
+    println("Here is a list of supplies in an ice cream shop:")
+    for (supply in iceCreamShopSupplies)
+    {
+        println(supply)
+    }
+
+}
